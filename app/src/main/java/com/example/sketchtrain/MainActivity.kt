c1bfa76eafa3fe1efca1995afcd38ce5f6ed8f11
Alexa <vivap41@gmail.com>
2024-05-13 00:21:43 +0200

    Primeras Pantallas (Inicio Sesión y Primeros pasos con menú lateral)

---
+++ app/src/main/java/com/example/sketchtrain/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.sketchtrain
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+    }
---
+++ app/src/main/java/com/example/sketchtrain/ui/newusers/FirstStep.kt
@@ -0,0 +1,30 @@
+package com.example.sketchtrain.ui.newusers
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.sketchtrain.databinding.FirstStepBinding
+class FirstStep : Fragment(){
+    private var _binding: FirstStepBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FirstStepBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+}}
